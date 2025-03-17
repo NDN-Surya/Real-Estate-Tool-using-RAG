6722a0e5f2dbbd06f6a06858770f5ccfe7a44354
NDNSurya <suryandn1618@gmail.com>
2025-03-18 01:37:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ from rag import process_urls, generate_answer
 
 st.title("Real Estate Research Tool")
 
+# Input URLs
 url1 = st.sidebar.text_input("URL 1")
 url2 = st.sidebar.text_input("URL 2")
 url3 = st.sidebar.text_input("URL 3")
 
+# Placeholder for displaying messages
 placeholder = st.empty()
 
+# Button to process URLs
 process_url_button = st.sidebar.button("Process URLs")
 if process_url_button:
-    urls = [url for url in (url1, url2, url3) if url!='']
+    urls = [url for url in (url1, url2, url3) if url != '']
     if len(urls) == 0:
-        placeholder.text("You must provide at least one valid url")
+        placeholder.text("You must provide at least one valid URL")
     else:
         for status in process_urls(urls):
             placeholder.text(status)
 
+# Input query for question-answering
 query = placeholder.text_input("Question")
 if query:
     try:
@@ -32,4 +36,5 @@ if query:
             for source in sources.split("\n"):
                 st.write(source)
     except RuntimeError as e:
-        placeholder.text("You must process urls first")
+        placeholder.text("You must process URLs first")
+
